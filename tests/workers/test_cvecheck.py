0532619ff45ed268e7dedbf0c5e117f44754afed
Jiri Popelka <jpopelka@redhat.com>
2017-06-16 16:36:19 +0200

    [TestCVEchecker] Python

--- tests/workers/test_cvecheck.py
+++ tests/workers/test_cvecheck.py
@@ -1,7 +1,7 @@
+import os
 import pytest
-
-from cucoslib.enums import EcosystemBackend
-from cucoslib.models import Ecosystem
+from flexmock import flexmock
+from cucoslib.object_cache import EPVCache
 from cucoslib.workers import CVEcheckerTask
 
 
@@ -43,5 +43,43 @@ class TestCVEchecker(object):
         # TODO: We'd need to download some jar to run the scan
         assert True
 
-    def test_python_scan(self):
-        assert True
+    def test_python_mako(self):
+        egg_info_dir = os.path.join(
+                        os.path.dirname(
+                         os.path.abspath(__file__)), '..', 'data', 'Mako.egg-info')
+        args = {'ecosystem': 'pypi', 'name': 'mako', 'version': '0.3.3'}
+        flexmock(EPVCache).should_receive('get_source_tarball').and_return('mako.tgz')
+        flexmock(EPVCache).should_receive('get_extracted_source_tarball').and_return(egg_info_dir)
+        task = CVEcheckerTask.create_test_instance(task_name='source_licenses')
+        results = task.execute(arguments=args)
+
+        assert isinstance(results, dict)
+        assert set(results.keys()) == {'details', 'status', 'summary'}
+        details = results.get('details')
+        #   "details": [
+        #        {
+        #            "cvss": {
+        #                "score": 4.3,
+        #                "vector": "AV:N/AC:M/Au:?/C:?/I:P/A:?"
+        #            },
+        #            "description": "Mako before 0.3.4 ...",
+        #            "id": "CVE-2010-2480",
+        #            "references": [
+        #                "http://www.makotemplates.org/CHANGES",
+        #                "http://bugs.python.org/issue9061",
+        #                "http://lists.opensuse.org/opensuse-security-announce/2010-08/msg00001.html"
+        #            ],
+        #            "severity": "Medium"
+        #        }
+        #    ]
+        assert isinstance(details, list) and len(details) == 1
+        detail = details[0]
+        assert set(detail.keys()) == {'cvss', 'description', 'id', 'references', 'severity'}
+        cvss = detail['cvss']
+        assert cvss['score'] == 4.3
+        assert cvss['vector'] == 'AV:N/AC:M/Au:?/C:?/I:P/A:?'
+        assert detail['id'] == 'CVE-2010-2480'
+        assert 'http://bugs.python.org/issue9061' in detail['references']
+        assert detail['severity'] == 'Medium'
+        assert results['status'] == 'success'
+        assert results['summary'] == ['CVE-2010-2480']
