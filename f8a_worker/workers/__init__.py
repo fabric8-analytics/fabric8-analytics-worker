ec27c2cc4e9c5276f599d61db6d7ab0b41b9384a
Samuzzal Choudhury <samuzzal@redhat.com>
2017-07-14 21:37:38 +0530

    added bookkeeper worker

--- f8a_worker/workers/__init__.py
+++ f8a_worker/workers/__init__.py
@@ -29,3 +29,4 @@ from f8a_worker.workers.recommender import RecommendationTask, RecommendationV2T
 from f8a_worker.workers.result_collector import ResultCollector, PackageResultCollector
 from f8a_worker.workers.stackaggregator import StackAggregatorTask
 from f8a_worker.workers.stackaggregator_v2 import StackAggregatorV2Task
+from f8a_worker.workers.bookkeeper import BookkeeperTask
