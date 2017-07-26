5a9f805a0a2fbae420234dc9c8876bb220287046
Jiri Popelka <jpopelka@redhat.com>
2017-07-26 17:38:13 +0200

    [licenses, scancode] Don't scan binary & media files

--- hack/patches/apply_patches.sh
+++ hack/patches/apply_patches.sh
@@ -4,4 +4,6 @@ HERE=$(dirname $0)
 
 patch -p1 --directory /usr/lib64/python3.4/site-packages/ < "${HERE}/0001-PyGithub.patch"
 
+patch -p1 --directory "${SCANCODE_PATH}" < "${HERE}/scancode_ignore_binary.patch"
+
 exit 0
