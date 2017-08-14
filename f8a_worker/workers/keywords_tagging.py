ce1bd621714d9bd59fba0998b52a0a80bead226c
Fridolin Pokorny <fridolin@redhat.com>
2017-08-14 14:06:54 +0200

    Download also stopwords for keyword tagging

--- f8a_worker/workers/keywords_tagging.py
+++ f8a_worker/workers/keywords_tagging.py
@@ -1,8 +1,8 @@
 """Perform keywords lookup on texts collected and available."""
 
+import os
 import requests
 from tempfile import NamedTemporaryFile
-from contextlib import contextmanager
 from datetime import timedelta
 from datetime import datetime
 
@@ -21,9 +21,13 @@ class KeywordsTaggingTask(BaseTask):
     # them directly from GitHub
     _keywords_yaml = {}
     _keywords_yaml_update = {}
-    _UPDATE_TIME = timedelta(minutes=15)
+    _UPDATE_TIME = timedelta(minutes=30)
+    _stopwords_txt = None
+    _stopwords_txt_update = None
     _TAGS_URL = 'https://raw.githubusercontent.com/fabric8-analytics/' \
                 'fabric8-analytics-tags/master/{ecosystem}_tags.yaml'
+    _STOPWORDS_URL = 'https://raw.githubusercontent.com/fabric8-analytics/' \
+                     'fabric8-analytics-tags/master/stopwords.txt'
 
     # Configuration of f8a_tagger lookup function - additional options. See f8a_tagger implementation for more details
     _LOOKUP_CONF = {
@@ -33,15 +37,14 @@ class KeywordsTaggingTask(BaseTask):
         'ngram_size': None
     }
 
-    @contextmanager
     def _get_keywords_yaml(self, ecosystem):
-        """Download keywords.yaml file if update time expires, otherwise use cached one.
+        """Download keywords.yaml file for the given ecosystem, use cached one if needed
 
-        :param ecosystem: ecosystem for which keywords file should be retrieved
-        :return: keywords.yaml file as stored on GitHub
+        :param ecosystem: ecosystem for which keywords.yaml file should be downloaded
+        :return: filename of downloaded keywords.yaml file
         """
         if self._keywords_yaml.get(ecosystem) is None \
-                or self._keywords_yaml_update[ecosystem] + self._UPDATE_TIME < datetime.now():
+                or self._keywords_yaml_update[ecosystem] < datetime.now():
             self.log.info("Updating keywords.yaml file for ecosystem '%s'", ecosystem)
 
             response = requests.get(self._TAGS_URL.format(ecosystem=ecosystem))
@@ -50,17 +53,54 @@ class KeywordsTaggingTask(BaseTask):
                                    % (ecosystem, response.status_code))
 
             if ecosystem in self._keywords_yaml:
-                self._keywords_yaml[ecosystem].close()
+                os.remove(self._keywords_yaml[ecosystem])
 
             fp = NamedTemporaryFile('r+', delete=False)
             fp.write(response.text)
             fp.close()
 
             self._keywords_yaml[ecosystem] = fp.name
-            self._keywords_yaml_update[ecosystem] = datetime.now()
-            self.log.info("File keywords.yaml file for ecosystem '%s'", ecosystem)
+            self._keywords_yaml_update[ecosystem] = datetime.now() + self._UPDATE_TIME
+            self.log.info("Downloaded file keywords.yaml for ecosystem '%s'", ecosystem)
 
-        yield self._keywords_yaml[ecosystem]
+        return self._keywords_yaml[ecosystem]
+
+    def _get_stopwords_txt(self):
+        """Get stopwords.txt file, use cached one if available.
+
+        :return: filename of downloaded stopwords.txt file
+        """
+        if self._stopwords_txt is None \
+                or self._stopwords_txt_update < datetime.now():
+            self.log.debug("Updating stopwords.txt file")
+
+            response = requests.get(self._STOPWORDS_URL)
+            if response.status_code != 200:
+                raise RuntimeError("Unable to download stopwords.txt file, HTTP status code is %s"
+                                   % response.status_code)
+
+            if self._stopwords_txt is not None:
+                os.remove(self._stopwords_txt)
+
+            fp = NamedTemporaryFile('r+', delete=False)
+            fp.write(response.text)
+            fp.close()
+
+            self._stopwords_txt = fp.name
+            self._stopwords_txt_update = datetime.now() + self._UPDATE_TIME
+            self.log.info("Downloaded stopwords.txt file")
+
+        return self._stopwords_txt
+
+    def _get_config_files(self, ecosystem):
+        """Download keywords.yaml and stopwords.txt files if update time expires, otherwise use cached ones.
+
+        :param ecosystem: ecosystem for which config files should be retrieved
+        :return: tuple with keywords.yaml and stopwords.txt file as stored on GitHub
+        """
+        keywords_yaml = self._get_keywords_yaml(ecosystem)
+        stopwords_txt = self._get_stopwords_txt()
+        return keywords_yaml, stopwords_txt
 
     def execute(self, arguments):
         # Keep f8a_tagger import local as other components dependent on f8a_worker do not require it installed.
@@ -74,26 +114,30 @@ class KeywordsTaggingTask(BaseTask):
         name = arguments['name']
 
         details = {}
-        with self._get_keywords_yaml(ecosystem) as keywords_file_name:
-            if 'metadata' in self.parent.keys():
-                self._strict_assert(arguments.get('version'))
+        keywords_file_name, stopwords_file_name = self._get_config_files(ecosystem)
+        if 'metadata' in self.parent.keys():
+            self._strict_assert(arguments.get('version'))
 
-                details['description'] = []
-                metadata = self.parent_task_result('metadata')
-                description = metadata.get('details', [{}])[0].get('description', '')
-                self.log.debug("Computing keywords from description: '%s'", description)
-                details['description'] = keywords_lookup_text(description, keywords_file=keywords_file_name,
-                                                              **self._LOOKUP_CONF)
+            details['description'] = []
+            metadata = self.parent_task_result('metadata')
+            description = metadata.get('details', [{}])[0].get('description', '')
+            self.log.debug("Computing keywords from description: '%s'", description)
+            details['description'] = keywords_lookup_text(description,
+                                                          keywords_file=keywords_file_name,
+                                                          stopwords_file=stopwords_file_name,
+                                                          **self._LOOKUP_CONF)
 
-                # explicitly gather declared keywords by publisher
-                self.log.debug("Aggregating explicitly stated keywords by publisher")
-                details['keywords'] = metadata.get('details', [{}])[0].get('keywords')
+            # explicitly gather declared keywords by publisher
+            self.log.debug("Aggregating explicitly stated keywords by publisher")
+            details['keywords'] = metadata.get('details', [{}])[0].get('keywords')
 
-            if 'GitReadmeCollectorTask' in self.parent.keys():
-                s3 = StoragePool.get_connected_storage('S3Readme')
-                readme_json = s3.retrieve_readme_json(ecosystem, name)
-                self.log.debug("Computing keywords from README.json")
-                details['README'] = keywords_lookup_readme(readme_json, keywords_file=keywords_file_name,
-                                                           **self._LOOKUP_CONF)
+        if 'GitReadmeCollectorTask' in self.parent.keys():
+            s3 = StoragePool.get_connected_storage('S3Readme')
+            readme_json = s3.retrieve_readme_json(ecosystem, name)
+            self.log.debug("Computing keywords from README.json")
+            details['README'] = keywords_lookup_readme(readme_json,
+                                                       keywords_file=keywords_file_name,
+                                                       stopwords_file=stopwords_file_name,
+                                                       **self._LOOKUP_CONF)
 
         return {'status': 'success', 'summary': [], 'details': details}
