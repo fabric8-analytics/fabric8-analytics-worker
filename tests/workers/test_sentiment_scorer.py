b119c1f149997615c9d3fc8bda35bcba35d83168
Fridolin Pokorny <fridolin@redhat.com>
2017-08-18 08:06:54 +0200

    Add dirty workaround for protobuf

--- tests/workers/test_sentiment_scorer.py
+++ tests/workers/test_sentiment_scorer.py
@@ -1,5 +1,6 @@
 class TestSentimentScorer(object):
     def test_import(self):
         # https://github.com/fabric8-analytics/fabric8-analytics-worker/issues/261
+        import google; import pkgutil; pkgutil.extend_path(google.__path__, google.__name__)
         from google.cloud import language
         from google.cloud import bigquery
