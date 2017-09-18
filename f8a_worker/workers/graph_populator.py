1fb55780ae99d97007e964b24b9c48b8e9978092
miteshvp <mitpatel@redhat.com>
2017-09-18 11:18:22 +0530

    safety against graphQL injection attack

--- f8a_worker/workers/graph_populator.py
+++ f8a_worker/workers/graph_populator.py
@@ -1,23 +1,38 @@
 import logging
 import re
 import time
-import json
 from datetime import datetime
 
 logger = logging.getLogger(__name__)
 
+# Compile patterns to clean a string (used for safety against GraphQL injection attack)
+danger_word_list = ["drop\(\)", "V\(\)", "count\(\)"]
+remove = '|'.join(danger_word_list)
+pattern = re.compile(r'(' + remove + ')', re.IGNORECASE)
+pattern_to_save = '[^\w\*\.Xx\-\>\=\<\~\^\|\/\:]'
+pattern_n2_remove = re.compile(pattern_to_save)
+
 
 class GraphPopulator(object):
     @classmethod
+    def str_value_cleaner(cls, safe_string):
+        """
+        Clean up the safe_string to remove suspicious words,
+        and special characters.
+        """
+        result = ''
+        if isinstance(safe_string, str):  # can raise an exception as well
+            temp_str = pattern.sub('', safe_string)
+            result = pattern_n2_remove.sub('', temp_str)
+        return result
+
+    @classmethod
     def construct_version_query(cls, input_json):
-        pkg_name = input_json.get('package')
-        ecosystem = input_json.get('ecosystem')
-        version = input_json.get('version')
         description = ''
         try:
             if len(input_json.get('analyses', {}).get('metadata', {}).get('details')) > 0:
                 description = input_json.get('analyses').get('metadata').get('details')[0].get('description', '')
-                description = description.replace("'", "\\'")
+                description = cls.str_value_cleaner(description).replace("'", "\\'")
         except:
             # we pass and move forward without description
             pass
@@ -30,13 +45,13 @@ class GraphPopulator(object):
         if 'success' == input_json.get('analyses', {}).get('security_issues', {}).get('status'):
             drop_props.append('cve_ids')
         if len(drop_props) > 0:
-            drop_prop += "g.V().has('pecosystem','" + ecosystem + "').has('pname','" + pkg_name + "')" \
-                         ".has('version','" + version + "').properties('" + "','".join(drop_props) + "')." \
+            drop_prop += "g.V().has('pecosystem', ecosystem).has('pname', pkg_name)" \
+                         ".has('version', version).properties('" + "','".join(drop_props) + "')." \
                          "drop().iterate();" \
 
-        str_version += "ver = g.V().has('pecosystem','" + ecosystem + "').has('pname','" + pkg_name + "')" \
-                       ".has('version','" + version + "').tryNext().orElseGet{graph.addVertex('pecosystem','" \
-                       + ecosystem + "', 'pname','" + pkg_name + "', 'version','" + version + "', " \
+        str_version += "ver = g.V().has('pecosystem', ecosystem).has('pname', pkg_name)" \
+                       ".has('version', version).tryNext().orElseGet{graph.addVertex('pecosystem', ecosystem," \
+                       "'pname', pkg_name, 'version', version, " \
                        "'vertex_label', 'Version')};" \
                        "ver.property('last_updated'," + str(time.time()) + ");"
         # Add Description if not blank
@@ -54,28 +69,28 @@ class GraphPopulator(object):
             cm_loc = str(input_json.get('analyses').get('code_metrics').get('summary', {}).get('total_lines', -1))
             cm_num_files = str(input_json.get('analyses').get('code_metrics').get('summary', {})
                                .get('total_files', -1))
-            prp_version += "ver.property('cm_num_files'," + cm_num_files + ");" \
-                           "ver.property('cm_avg_cyclomatic_complexity'," + cm_avg_cyclomatic_complexity + ");" \
-                           "ver.property('cm_loc'," + str(cm_loc) + ");"
+            prp_version += "ver.property('cm_num_files'," + cls.str_value_cleaner(cm_num_files) + ");" \
+                           "ver.property('cm_avg_cyclomatic_complexity'," + cls.str_value_cleaner(cm_avg_cyclomatic_complexity) + ");" \
+                           "ver.property('cm_loc'," + cls.str_value_cleaner(str(cm_loc)) + ");"
 
         # Get downstream details
 
         if len(input_json.get('analyses', {}).get('redhat_downstream', {})
                .get('summary', {}).get('all_rhsm_product_names', [])) > 0:
             shipped_as_downstream = 'true'
-            prp_version += "ver.property('shipped_as_downstream'," + shipped_as_downstream + ");"
+            prp_version += "ver.property('shipped_as_downstream'," + cls.str_value_cleaner(shipped_as_downstream) + ");"
 
         # Add license details
         if 'source_licenses' in input_json.get('analyses', {}):
             licenses = input_json.get('analyses').get('source_licenses').get('summary', {}).get('sure_licenses', [])
-            prp_version += " ".join(map(lambda x: "ver.property('licenses', '" + x + "');", licenses))
+            prp_version += " ".join(map(lambda x: "ver.property('licenses', '" + cls.str_value_cleaner(x) + "');", licenses))
 
         # Add CVE property if it exists
         if 'security_issues' in input_json.get('analyses', {}):
             cves = []
             for cve in input_json.get('analyses', {}).get('security_issues', {}).get('details', []):
                 cves.append(cve.get('id') + ":" + str(cve.get('cvss', {}).get('score')))
-            prp_version += " ".join(map(lambda x: "ver.property('cve_ids', '" + x + "');", cves))
+            prp_version += " ".join(map(lambda x: "ver.property('cve_ids', '" + cls.str_value_cleaner(x) + "');", cves))
 
         # Get Metadata Details
         if 'metadata' in input_json.get('analyses', {}):
@@ -89,7 +104,8 @@ class GraphPopulator(object):
                     # string with comma separated license names
                     declared_licenses = details[0]['declared_license'].split(',')
 
-                prp_version += " ".join(map(lambda x: "ver.property('declared_licenses', '" + x + "');", declared_licenses))
+                prp_version += " ".join(map(lambda x: "ver.property('declared_licenses', '" + cls.str_value_cleaner(x) \
+                                                      + "');", declared_licenses))
                 # Create License Node and edge from EPV
                 for lic in declared_licenses:
                     prp_version += "lic = g.V().has('lname','" + lic + "').tryNext()." \
@@ -104,12 +120,10 @@ class GraphPopulator(object):
 
     @classmethod
     def construct_package_query(cls, input_json):
-        pkg_name = input_json.get('package')
-        ecosystem = input_json.get('ecosystem')
-        pkg_name_tokens = re.split('\W+', pkg_name)
+        pkg_name_tokens = re.split('\W+', input_json.get('package'))
         prp_package = ""
-        str_package = "pkg = g.V().has('ecosystem','" + ecosystem + "').has('name','" + pkg_name + "').tryNext()" \
-                      ".orElseGet{graph.addVertex('ecosystem', '" + ecosystem + "', 'name', '" + pkg_name + "', " \
+        str_package = "pkg = g.V().has('ecosystem', ecosystem).has('name', pkg_name).tryNext()" \
+                      ".orElseGet{graph.addVertex('ecosystem', ecosystem, 'name', pkg_name, " \
                       "'vertex_label', 'Package')};" \
                       "pkg.property('last_updated', " + str(time.time()) + ");"
 
@@ -133,23 +147,23 @@ class GraphPopulator(object):
             gh_open_issues_count = str(gh_details.get('open_issues_count', -1))
             gh_subscribers_count = str(gh_details.get('subscribers_count', -1))
 
-            prp_package += "pkg.property('gh_prs_last_year_opened', " + gh_prs_last_year_opened + ");" \
-                           "pkg.property('gh_prs_last_month_opened', " + gh_prs_last_month_opened + ");" \
-                           "pkg.property('gh_prs_last_year_closed', " + gh_prs_last_year_closed + ");" \
-                           "pkg.property('gh_prs_last_month_closed', " + gh_prs_last_month_closed + ");" \
-                           "pkg.property('gh_issues_last_year_opened', " + gh_issues_last_year_opened + ");" \
-                           "pkg.property('gh_issues_last_month_opened', " + gh_issues_last_month_opened + ");" \
-                           "pkg.property('gh_issues_last_year_closed', " + gh_issues_last_year_closed + ");" \
-                           "pkg.property('gh_issues_last_month_closed', " + gh_issues_last_month_closed + ");" \
-                           "pkg.property('gh_forks', " + gh_forks + ");" \
-                           "pkg.property('gh_stargazers', " + gh_stargazers + ");" \
-                           "pkg.property('gh_open_issues_count', " + gh_open_issues_count + ");" \
-                           "pkg.property('gh_subscribers_count', " + gh_subscribers_count + ");"
+            prp_package += "pkg.property('gh_prs_last_year_opened', " + cls.str_value_cleaner(gh_prs_last_year_opened) + ");" \
+                           "pkg.property('gh_prs_last_month_opened', " + cls.str_value_cleaner(gh_prs_last_month_opened) + ");" \
+                           "pkg.property('gh_prs_last_year_closed', " + cls.str_value_cleaner(gh_prs_last_year_closed) + ");" \
+                           "pkg.property('gh_prs_last_month_closed', " + cls.str_value_cleaner(gh_prs_last_month_closed) + ");" \
+                           "pkg.property('gh_issues_last_year_opened', " + cls.str_value_cleaner(gh_issues_last_year_opened) + ");" \
+                           "pkg.property('gh_issues_last_month_opened', " + cls.str_value_cleaner(gh_issues_last_month_opened) + ");" \
+                           "pkg.property('gh_issues_last_year_closed', " + cls.str_value_cleaner(gh_issues_last_year_closed) + ");" \
+                           "pkg.property('gh_issues_last_month_closed', " + cls.str_value_cleaner(gh_issues_last_month_closed) + ");" \
+                           "pkg.property('gh_forks', " + cls.str_value_cleaner(gh_forks) + ");" \
+                           "pkg.property('gh_stargazers', " + cls.str_value_cleaner(gh_stargazers) + ");" \
+                           "pkg.property('gh_open_issues_count', " + cls.str_value_cleaner(gh_open_issues_count) + ");" \
+                           "pkg.property('gh_subscribers_count', " + cls.str_value_cleaner(gh_subscribers_count) + ");"
 
         # Add tokens for a package
         for tkn in pkg_name_tokens:
             if tkn:
-                str_package += "pkg.property('tokens', '" + tkn + "');"
+                str_package += "pkg.property('tokens', '" + cls.str_value_cleaner(tkn) + "');"
 
         # Get Libraries.io data
         if 'libraries_io' in input_json.get('analyses', {}):
@@ -177,22 +191,22 @@ class GraphPopulator(object):
                                       .get('dependent_repositories', {}).get('top', {}).items():
                 prp_package += "pkg.property('libio_usedby', '" + key + ":" + val + "');"
 
-            prp_package += "pkg.property('libio_dependents_projects', '" + libio_dependents_projects + "');" \
-                           "pkg.property('libio_dependents_repos', '" + libio_dependents_repos + "');" \
-                           "pkg.property('libio_total_releases', '" + libio_total_releases + "');" \
-                           "pkg.property('libio_latest_version', '" + libio_latest_version + "');"
+            prp_package += "pkg.property('libio_dependents_projects', '" + cls.str_value_cleaner(libio_dependents_projects) + "');" \
+                           "pkg.property('libio_dependents_repos', '" + cls.str_value_cleaner(libio_dependents_repos) + "');" \
+                           "pkg.property('libio_total_releases', '" + cls.str_value_cleaner(libio_total_releases) + "');" \
+                           "pkg.property('libio_latest_version', '" + cls.str_value_cleaner(libio_latest_version) + "');"
 
             # Update EPV Github Release Date based on libraries_io data
             try:
                 if libio_latest_release:
-                    prp_package += "g.V().has('pecosystem','" + ecosystem + "').has('pname','" + pkg_name + "')." \
-                                   "has('version','" + libio_latest_version + "')." \
+                    prp_package += "g.V().has('pecosystem', ecosystem).has('pname', pkg_name)." \
+                                   "has('version','" + cls.str_value_cleaner(libio_latest_version) + "')." \
                                    "property('gh_release_date'," + str(time.mktime(datetime.strptime(libio_latest_release,
                                                                        '%b %d, %Y').timetuple())) + ");"
                 for version, release in input_json.get('analyses').get('libraries_io').get('details', {}) \
                                                   .get('releases', {}).get('latest', {}).get('recent', {}).items():
-                    prp_package += "g.V().has('pecosystem','" + ecosystem + "').has('pname','" + pkg_name + "')." \
-                                   "has('version','" + version + "')." \
+                    prp_package += "g.V().has('pecosystem', ecosystem).has('pname', pkg_name)." \
+                                   "has('version', version)." \
                                    "property('gh_release_date'," + str(time.mktime(datetime.strptime(release,
                                                                        '%b %d, %Y').timetuple())) + ");"
             except:
@@ -204,27 +218,24 @@ class GraphPopulator(object):
     @classmethod
     def create_query_string(cls, input_json):
 
-        pkg_name = input_json.get('package')
-        ecosystem = input_json.get('ecosystem')
-        version = input_json.get('version')
         # creation of query string
         str_gremlin = ""
         str_package, prp_package = cls.construct_package_query(input_json)
         if prp_package:
             str_gremlin = str_package + prp_package
 
-        if version is not None and version != '':
+        if input_json.get('version') is not None and input_json.get('version') != '':
             str_gremlin_version = cls.construct_version_query(input_json)
             # Add edge from Package to Version
             if str_gremlin_version:
                 str_gremlin += str_gremlin_version
 
             # Add Package -> Version edge
-            str_gremlin += "g.V().has('pecosystem','" + ecosystem + "').has('pname','" + pkg_name + \
-                           "').has('version','" + version + "').in('has_version').tryNext()" \
-                           ".orElseGet{pkg=g.V().has('ecosystem','" + ecosystem + "').has('name','" + pkg_name + "')" \
-                           ".tryNext().orElseGet{graph.addVertex('ecosystem','" + ecosystem + "', 'name','" \
-                           + pkg_name + "', 'vertex_label', 'Package')};g.V(pkg).next().addEdge('has_version',ver)};"
+            str_gremlin += "g.V().has('pecosystem', ecosystem).has('pname', pkg_name)." \
+                           "has('version', version).in('has_version').tryNext()" \
+                           ".orElseGet{pkg=g.V().has('ecosystem', ecosystem).has('name', pkg_name)" \
+                           ".tryNext().orElseGet{graph.addVertex('ecosystem', ecosystem, 'name', pkg_name, " \
+                           "'vertex_label', 'Package')};g.V(pkg).next().addEdge('has_version',ver)};"
 
         logger.debug(str_gremlin)
 
