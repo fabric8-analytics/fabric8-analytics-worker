799fae3d59445f106a92fd6cd3a70339e0e094c6
Saket Kumar <skumar@localhost.localdomain>
2017-10-03 16:41:36 +0530

    remove test_sentiment_scorer.py

--- tests/workers/test_sentiment_scorer.py
+++
@@ -1,5 +0,0 @@
-class TestSentimentScorer(object):
-    def test_import(self):
-        # https://github.com/fabric8-analytics/fabric8-analytics-worker/issues/261
-        from google.cloud import language
-        from google.cloud import bigquery
