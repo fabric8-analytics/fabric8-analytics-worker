9c49dbe7df229ecce51b5832194a0506085ecba8
Michal Srb <michal@redhat.com>
2017-10-04 22:32:51 +0200

    Handle gofedlib in data normalizer

--- tests/test_data_normalizer.py
+++ tests/test_data_normalizer.py
@@ -284,3 +284,18 @@ class TestDataNormalizer(object):
             transformed_value = sorted(transformed_data[key]) \
                 if isinstance(transformed_data[key], list) else transformed_data[key]
             assert transformed_value == value
+
+    @pytest.mark.parametrize('data, expected', [
+        ({'ecosystem': 'gofedlib', 'result': {'deps-main': [],
+                                              'deps-packages': ['https://github.com/gorilla/context']}},
+         {'ecosystem': 'gofedlib', 'dependencies': ['github.com/gorilla/context']}),
+        ({'ecosystem': 'gofedlib',
+          'result': {'deps-main': ['https://github.com/gorilla/sessions', 'https://github.com/gorilla/context'],
+                     'deps-packages': ['https://github.com/gorilla/context']}},
+         {'ecosystem': 'gofedlib', 'dependencies': ['github.com/gorilla/sessions', 'github.com/gorilla/context']}),
+    ])
+    def test_transforming_gofedlib_data(self, data, expected):
+        transformed_data = self._dataNormalizer.handle_data(data)
+        for key, value in expected.items():
+            assert key in transformed_data
+            assert transformed_data[key] == value
