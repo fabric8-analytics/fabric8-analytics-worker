a0b943ce2f6cd3063f72afd19ef33d94d8d33251
Saleem Ansari <tuxdna@gmail.com>
2017-10-16 15:02:44 +0530

    Fix issue 298 - Use long flag instead of -s

--- f8a_worker/workers/mercator.py
+++ f8a_worker/workers/mercator.py
@@ -197,7 +197,7 @@ class MercatorTask(BaseTask):
         if arguments['ecosystem'] == 'go':
             # no Go support in Mercator-go yet, we handle it separately here
             tc = TimedCommand(['gofedlib-cli', '--dependencies-main', '--dependencies-packages',
-                               '--dependencies-test', '-s',
+                               '--dependencies-test', '--skip-errors',
                                mercator_target])
             status, data, err = tc.run(timeout=timeout)
         else:
