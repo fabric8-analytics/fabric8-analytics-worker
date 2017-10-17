9fa988ebddfcef41825e764701b340279a8ceb5a
Jyasveer Gotta <jyasveer@gmail.com>
2017-10-17 18:33:31 +0530

    fix review comments

--- f8a_worker/storages/s3_manual_tagging.py
+++ f8a_worker/storages/s3_manual_tagging.py
@@ -3,22 +3,23 @@ from . import AmazonS3
 
 class S3ManualTagging(AmazonS3):
     def store_user_data(self, input_json):
-        file_name = self.get_file_name(input_json.get('user'))
+        if input_json.get('user') is None or input_json.get('ecosystem') is None:
+            return {'status': 'failure'}
+        file_name = self.get_file_name(input_json.get('user'), input_json.get('ecosystem'))
         data = input_json.get('data', {})
-        ecosystem = data.get('ecosystem', None)
-        file_name = ecosystem + "/" + file_name
         self.store_dict(data, file_name)
+        return {'status': 'success'}
 
     def fetch_user_data(self, user, ecosystem):
-        user_json = {}
-        file_name = self.get_file_name(user)
-        file_name = ecosystem + "/" + file_name
+        file_name = self.get_file_name(user, ecosystem)
         file_data = self.retrieve_dict(file_name)
-        user_json['user'] = input_json['user']
-        user_json['data'] = file_data
+        user_json = {
+            'user': user,
+            'data': file_data
+        }
         return user_json
 
-    def get_file_name(self, user):
+    def get_file_name(self, user, ecosystem):
         postfix = "_user.json"
-        file_name = user + postfix
+        file_name = ecosystem + "/" + user + postfix
         return file_name
