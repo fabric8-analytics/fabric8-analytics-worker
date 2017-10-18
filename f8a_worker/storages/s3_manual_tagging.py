7d8b994c4179316f42cfef1f53801f30a0eb5604
Jyasveer Gotta <jyasveer@gmail.com>
2017-10-18 13:40:33 +0530

    fix review comments

--- f8a_worker/storages/s3_manual_tagging.py
+++ f8a_worker/storages/s3_manual_tagging.py
@@ -3,12 +3,13 @@ from . import AmazonS3
 
 class S3ManualTagging(AmazonS3):
     def store_user_data(self, input_json):
-        if input_json.get('user') is None or input_json.get('ecosystem') is None:
-            return {'status': 'failure'}
-        file_name = self.get_file_name(input_json.get('user'), input_json.get('ecosystem'))
+        if 'user' not in input_json or 'data' not in input_json:
+            raise ValueError("User and data are needed in supplied JSON, got "
+                             "{} instead".format(input_json))
+        file_name = self.get_file_name(input_json.get('user'),
+                                       input_json.get('data').get('ecosystem'))
         data = input_json.get('data', {})
         self.store_dict(data, file_name)
-        return {'status': 'success'}
 
     def fetch_user_data(self, user, ecosystem):
         file_name = self.get_file_name(user, ecosystem)
