07a9d7c7dca7d5089be9f2e6a4ad9a7f81d00ef9
Jiri Popelka <jpopelka@redhat.com>
2017-10-31 16:37:12 +0100

    [S3VulnDB] store/retrieve Victims CVE DB

--- f8a_worker/storages/s3_vulndb.py
+++ f8a_worker/storages/s3_vulndb.py
@@ -3,13 +3,15 @@ from datetime import datetime, timezone
 import os
 from f8a_worker.errors import TaskError
 from f8a_worker.process import Archive
-from f8a_worker.utils import tempdir
+from f8a_worker.utils import cwd, tempdir
 from . import AmazonS3
 
 
 class S3VulnDB(AmazonS3):
     _DEPCHECK_DB_FILENAME = 'dc.h2.db'
     _DEPCHECK_DB_ARCHIVE = _DEPCHECK_DB_FILENAME + '.zip'
+    _VICTIMS_DB_DIR = 'victims-cve-db'
+    _VICTIMS_DB_ARCHIVE = _VICTIMS_DB_DIR + '.zip'
 
     _METAINFO_OBJECT_KEY = 'meta.json'
 
@@ -53,6 +55,28 @@ class S3VulnDB(AmazonS3):
             with tempdir() as archive_dir:
                 archive_path = os.path.join(archive_dir, self._DEPCHECK_DB_ARCHIVE)
                 self.retrieve_file(self._DEPCHECK_DB_ARCHIVE, archive_path)
+                Archive.extract_zip(archive_path, data_dir)
+                return True
+        return False
+
+    def store_victims_db(self, data_dir):
+        """ Zip Victims CVE DB and store to S3 """
+        with tempdir() as archive_dir:
+            archive_path = os.path.join(archive_dir, self._VICTIMS_DB_ARCHIVE)
+            with cwd(data_dir):
+                try:
+                    Archive.zip_file(self._VICTIMS_DB_DIR, archive_path)
+                except TaskError:
+                    pass
+                else:
+                    self.store_file(archive_path, self._VICTIMS_DB_ARCHIVE)
+
+    def retrieve_victims_db_if_exists(self, data_dir):
+        """ Retrieve zipped Victims CVE DB file as stored on S3 and extract"""
+        if self.object_exists(self._VICTIMS_DB_ARCHIVE):
+            with tempdir() as archive_dir:
+                archive_path = os.path.join(archive_dir, self._VICTIMS_DB_ARCHIVE)
+                self.retrieve_file(self._VICTIMS_DB_ARCHIVE, archive_path)
                 Archive.extract_zip(archive_path, data_dir)
                 return True
         return False
