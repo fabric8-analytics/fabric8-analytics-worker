f558809a26035c82869439bbf888320075d0f6ec
Pavel Kajaba <pavel@redhat.com>
2017-11-02 22:21:34 +0100

    Remove unused run of pylint inside image with tests

--- hack/exec_tests.sh
+++ hack/exec_tests.sh
@@ -6,6 +6,3 @@ DIR=$(dirname "${BASH_SOURCE[0]}")
 # we need no:cacheprovider, otherwise pytest will try to write to directory .cache which is in /usr under unprivileged
 # user and will cause exception
 py.test -p no:cacheprovider -vv $@
-
-echo "Running pylint..."
-pylint ./f8a_worker/ > /tmp/pylint.log || exit 0
