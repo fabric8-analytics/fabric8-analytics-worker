f1d8dbcc24124842187dee07d24a01743a2ae5d1
Fridolin Pokorny <fridolin@redhat.com>
2017-11-10 12:42:36 +0100

    Add file for generating flow migrations

---
+++ hack/create_flow_migration.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DISPATCHER_CONF_DIR=${DISPATCHER_CONF_DIR:-'../f8a_worker/dispatcher/'}
+MIGRATION_DIR=${DISPATCHER_CONF_DIR:-'../f8a_worker/dispatcher/migration_dir'}
+
+which selinonlib-cli 2>/dev/null >&1 || {
+    echo 'Please install selinonlib-cli to visualize flow by running `pip3 install selinonlib`'
+    exit 1
+}
+
+# As we are using custom predicates and we want to make sure that the queue
+# expansion name is done correctly, export expected env vars.
+PYTHONPATH='../' DEPLOYMENT_PREFIX='plot_' WORKER_ADMINISTRATION_REGION="api" \
+    selinonlib-cli -vvvv migrate --nodes-definition "${DISPATCHER_CONF_DIR}/nodes.yml" \
+                        --flow-definitions "${DISPATCHER_CONF_DIR}"/flows/*.yml \
+			--git --migration-dir "${MIGRATION_DIR}" &&\
+                        echo "Flow migration is present in the migration dir, don't forget to commit it with your flow changes."
