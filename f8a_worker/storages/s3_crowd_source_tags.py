f8c56b0778a64752f8ff330c13c6d9d184586401
Saket Kumar <skumar@localhost.localdomain>
2017-11-21 11:57:40 +0530

    update for reviews

--- f8a_worker/storages/s3_crowd_source_tags.py
+++ f8a_worker/storages/s3_crowd_source_tags.py
@@ -3,7 +3,7 @@
 from . import AmazonS3
 
 
-class S3GitHubManifestMetadata(AmazonS3):
+class S3CrowdSourceTags(AmazonS3):
 
     def _construct_object_key(self, arguments):
         path = self.get_object_key_path(arguments['ecosystem'])
@@ -17,9 +17,5 @@ class S3GitHubManifestMetadata(AmazonS3):
         assert 'ecosystem' in node_args
 
         object_key = self._construct_object_key(node_args)
-        version_id = self.store_dict(result[1], object_key)
-        return version_id
-
-    def store_raw_manifest(self, ecosystem, repo_path, filename, manifest):
-        path = self.get_object_key_path(ecosystem, repo_path)
-        self.store_blob(manifest, "{path}/{filename}".format(path=path, filename=filename))
+        self.store_dict(result[1], object_key)
+        return
