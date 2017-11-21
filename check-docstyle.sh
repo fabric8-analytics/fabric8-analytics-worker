cdea839005eb9120a7bbd7b087d1f35341b79bf5
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-11-21 15:23:03 +0100

    Refactored function to run pydocscheck against given list of files

--- check-docstyle.sh
+++ check-docstyle.sh
@@ -13,21 +13,9 @@ function prepare_venv() {
     ${VIRTUALENV} -p python3 venv && source venv/bin/activate && python3 `which pip3` install pydocstyle
 }
 
-echo "----------------------------------------------------"
-echo "Checking documentation strings in all sources stored"
-echo "in following directories:"
-echo $directories
-echo "----------------------------------------------------"
-echo
-
-[ "$NOVENV" == "1" ] || prepare_venv || exit 1
-
-# checks for the whole directories
-for directory in $directories
-do
-    files=`find $directory -path $directory/venv -prune -o -name '*.py' -print`
-
-    for source in $files
+# run the pydocstyle for all files that are provided in $1
+function check_files() {
+    for source in $1
     do
         echo $source
         pydocstyle --count $source
@@ -44,6 +32,24 @@ do
             let "fail++"
         fi
     done
+}
+
+
+echo "----------------------------------------------------"
+echo "Checking documentation strings in all sources stored"
+echo "in following directories:"
+echo $directories
+echo "----------------------------------------------------"
+echo
+
+[ "$NOVENV" == "1" ] || prepare_venv || exit 1
+
+# checks for the whole directories
+for directory in $directories
+do
+    files=`find $directory -path $directory/venv -prune -o -name '*.py' -print`
+
+    check_files "$files"
 done
 
 
@@ -53,24 +59,7 @@ echo "Checking documentation strings in the following files"
 echo $separate_files
 echo "----------------------------------------------------"
 
-# check for individual files
-for source in $separate_files
-do
-    echo $source
-    pydocstyle --count $source
-    if [ $? -eq 0 ]
-    then
-        echo "    Pass"
-        let "pass++"
-    elif [ $? -eq 2 ]
-    then
-        echo "    Illegal usage (should not happen)"
-        exit 2
-    else
-        echo "    Fail"
-        let "fail++"
-    fi
-done
+check_files "$separate_files"
 
 
 if [ $fail -eq 0 ]
