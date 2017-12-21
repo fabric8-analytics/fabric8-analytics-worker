a1ec41cd4ab53a18971bf65898f8159a7ef96b6d
Samuzzal Choudhury <samuzzal@redhat.com>
2017-12-21 17:01:16 +0530

    Changed prefetch multiplier to 1

--- hack/workers.sh
+++ hack/workers.sh
@@ -6,4 +6,4 @@ source worker-queues-env.sh
 
 # Keep celery worker as minimal as possible to avoid sending messages that we don't really care about
 # Also keep prefetch equal to 0 as boto library hangs in an infinite loop when prefetch is set to non-zero
-exec celery worker -P solo -A f8a_worker.start -Q "${WORKER_QUEUES}" --concurrency=1 --prefetch-multiplier=128 -Ofair --without-gossip --without-mingle --without-heartbeat
+exec celery worker -P solo -A f8a_worker.start -Q "${WORKER_QUEUES}" --concurrency=1 --prefetch-multiplier=1 -Ofair --without-gossip --without-mingle --without-heartbeat
