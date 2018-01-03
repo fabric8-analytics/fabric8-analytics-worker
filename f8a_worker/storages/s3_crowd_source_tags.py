7c15fc196008800ac1b55c5dd3fcb574ffb5dbc1
Jiri Popelka <jpopelka@redhat.com>
2018-01-03 19:27:22 +0100

    [storages/S3CrowdSourceTags] retrieve package_topic.json

--- f8a_worker/storages/s3_crowd_source_tags.py
+++ f8a_worker/storages/s3_crowd_source_tags.py
@@ -1,21 +1,17 @@
-#!/usr/bin/env python3
-
 from . import AmazonS3
 
 
 class S3CrowdSourceTags(AmazonS3):
-
-    def _construct_object_key(self, arguments):
-        path = self.get_object_key_path(arguments['ecosystem'])
-        return "{path}/crowd_sourcing_package_topic.json".format(path=path)
-
     @staticmethod
     def get_object_key_path(ecosystem):
         return "{ecosystem}/github/data_input_raw_package_list/".format(ecosystem=ecosystem)
 
-    def store(self, node_args, flow_name, task_name, task_id, result):
-        assert 'ecosystem' in node_args
+    def retrieve_package_topic(self, ecosystem):
+        path = self.get_object_key_path(ecosystem)
+        object_key = "{path}/package_topic.json".format(path=path)
+        return self.retrieve_dict(object_key)
 
-        object_key = self._construct_object_key(node_args)
-        self.store_dict(result, object_key)
-        return
+    def store_package_topic(self, ecosystem, results):
+        path = self.get_object_key_path(ecosystem)
+        object_key = "{path}/crowd_sourcing_package_topic.json".format(path=path)
+        self.store_dict(results, object_key)
