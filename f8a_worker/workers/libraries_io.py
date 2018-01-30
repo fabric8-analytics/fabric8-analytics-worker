e15ef1b1867a8d9208c897304d04441f4ae1e8a8
Jiri Popelka <jpopelka@redhat.com>
2018-01-30 15:54:44 +0100

    [LibrariesIoTask] Use Libraries.io API + add schema

--- f8a_worker/workers/libraries_io.py
+++ f8a_worker/workers/libraries_io.py
@@ -1,103 +1,81 @@
+"""Task to collects statistics from Libraries.io."""
+
 from bs4 import BeautifulSoup
-from re import compile as re_compile
+from operator import itemgetter
 from requests import get
 from urllib.parse import quote
 
 from f8a_worker.base import BaseTask
+from f8a_worker.errors import TaskError
+from f8a_worker.utils import get_response
 from f8a_worker.schemas import SchemaRef
 
 
 class LibrariesIoTask(BaseTask):
-    """ Collects statistics from Libraries.io """
+    """Collects statistics from Libraries.io."""
     _analysis_name = "libraries_io"
-    # schema_ref = SchemaRef(_analysis_name, '1-0-0')
+    schema_ref = SchemaRef(_analysis_name, '2-0-0')
 
     @staticmethod
-    def _get_list_term_description(page, term_name):
-        tag = page.find(string=re_compile(r'^\s*{}\s*$'.format(term_name)))
-        if not tag:
-            return None
-        return tag.find_next('dd')
+    def get_top_dependent_repositories(ecosystem, name):
+        """Return content of https://libraries.io/{ecosystem}/{name}/top_dependent_repos as dict.
 
-    def _get_list_term_description_text(self, page, term_name):
-        term_description = self._get_list_term_description(page, term_name)
-        result = 'n/a'
-        if term_description:
-            result = term_description.text.strip()
-        return result
+        There's no API equivalent of this page, but the page is very simple, we take
+        everything from it and return as dict of <repository>: <number of stars>
+        """
+        url = 'https://libraries.io/{ecosystem}/{name}/top_dependent_repos'.\
+            format(ecosystem=ecosystem, name=name)
+        page = BeautifulSoup(get(url).text, 'html.parser')
+        top_dep_repos = {tag.text.strip(): int(tag.find_next('dd').text.strip())
+                         for tag in page.find_all(['dt'])}
+        return top_dep_repos
 
-    def _get_list_term_description_time(self, page, term_name):
-        term_description = self._get_list_term_description(page, term_name)
-        result = 'n/a'
-        if term_description:
-            result = term_description.find('time').get('datetime')
-        return result
+    def project_url(self, ecosystem, name):
+        """Construct url to endpoint, which gets information about a project and it's versions."""
+        url = '{api}/{platform}/{name}?api_key={token}'.\
+            format(api=self.configuration.LIBRARIES_IO_API,
+                   platform=ecosystem,
+                   name=name,
+                   token=self.configuration.LIBRARIES_IO_TOKEN)
+        return url
 
-    def get_releases(self, page):
-        latest_version = ''
-        latest_published_at = ''
-        recent = {}
-
-        releases_tag = page.find_all('div', class_="col-md-4 sidebar")[-1]
-        vers = releases_tag.find_all('dt')
-        if vers:
-            # Releases
-            latest_version = vers[0].find('a').text.strip()
-            latest_published_at = vers[0].find_next('dd').text.strip()
-            recent = {v.find('a').text.strip(): v.find_next('dd').text.strip() for v in vers[1:]}
-        else:
-            # Tagged Releases, e.g. https://libraries.io/pypi/osbs-client/
-            vers = releases_tag.find_all('dl')[1:]
-            if vers:
-                latest_version = vers[0].text.strip()
-                latest_published_at = vers[0].find_next('dd').text.strip()
-                recent = {v.text.strip(): v.find_next('dd').text.strip() for v in vers[1:]}
-
-        releases = {'count': self._get_list_term_description_text(page, 'Total releases'),
-                    'latest': {'version': latest_version,
-                               'published_at': latest_published_at,
-                               'recent': recent}}
-        return releases
-
-    def get_dependents(self, page):
-        dependents = {'count': self._get_list_term_description_text(page, 'Dependent projects')}
-        return dependents
-
-    def get_dependent_repositories(self, page, top_dependent_repos_page):
-        # dict of <repository>: <number of stars> from
-        # https://libraries.io/{ecosystem}/{name}/top_dependent_repos
-        top_dep_repos = {tag.text.strip(): tag.find_next('dd').text.strip()
-                         for tag in top_dependent_repos_page.find_all(['dt'])}
-        dependent_repos = {'count': self._get_list_term_description_text(page,
-                                                                         'Dependent repositories'),
-                           'top': top_dep_repos}
-        return dependent_repos
+    @staticmethod
+    def recent_releases(versions, count=10):
+        """Sort versions by 'published_at' and return 'count' latest."""
+        return sorted(versions, key=itemgetter('published_at'))[-count:]
 
     def execute(self, arguments):
+        """Task entrypoint."""
         self._strict_assert(arguments.get('ecosystem'))
         self._strict_assert(arguments.get('name'))
+
+        result_data = {'status': 'unknown',
+                       'summary': [],
+                       'details': {}}
 
         name = arguments['name']
         ecosystem = arguments['ecosystem']
         if ecosystem == 'go':
             name = quote(name, safe='')
 
-        url = 'https://libraries.io/{ecosystem}/{name}'.format(ecosystem=ecosystem,
-                                                               name=name)
-        page = BeautifulSoup(get(url).text, 'html.parser')
-        if page.find(text="We can't find whatever it was you were looking for."):
-            self.log.warning('%r does not exist' % url)
-            return {'status': 'error',
-                    'summary': ['{url} does not exist'.format(url=url)],
-                    'details': {}}
+        try:
+            project = get_response(self.project_url(ecosystem, name))
+        except TaskError as e:
+            self.log.debug(e)
+            result_data['status'] = 'error'
+            return result_data
 
-        top_dependent_repos_page = BeautifulSoup(get(url + '/top_dependent_repos').text,
-                                                 'html.parser')
-
-        details = {'releases': self.get_releases(page),
-                   'dependents': self.get_dependents(page),
-                   'dependent_repositories': self.get_dependent_repositories(
-                       page, top_dependent_repos_page)}
+        versions = project['versions']
+        details = {'dependent_repositories': {'count': project['dependent_repos_count'],
+                                              'top': self.get_top_dependent_repositories(ecosystem,
+                                                                                         name)},
+                   'dependents': {'count': project['dependents_count']},
+                   'releases': {'count': len(versions),
+                                # 'latest': {'version': project['latest_release_number'],
+                                #           'published_at': project['latest_release_published_at']},
+                                'recent': self.recent_releases(versions)
+                                }
+                   }
 
         return {'status': 'success',
                 'summary': [],
