edae020dd20fb9aa0d169ffa32e4e57efb52ba72
Geetika Batra <geetika791@gmail.com>
2018-03-20 13:52:11 +0530

    Adding support for sentry

--- f8a_worker/start.py
+++ f8a_worker/start.py
@@ -2,8 +2,21 @@
 
 """Start the application."""
 
-from celery import Celery
+import celery
+import os
 from f8a_worker.setup_celery import init_celery, init_selinon
+from raven.contrib.celery import register_signal, register_logger_signal
+
+
+class Celery(celery.Celery):
+    """Celery class to configure sentry."""
+
+    def on_configure(self):
+        """Set up sentry client."""
+        dsn = os.environ.get("SENTRY_DSN")
+        client = raven.Client(dsn)
+        register_logger_signal(client)
+        register_signal(client)
 
 
 app = Celery('tasks')
