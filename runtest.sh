bf33c4921dabd03e8182e26de8978e0a951f5e21
Michal Srb <michal@redhat.com>
2018-04-10 18:08:19 +0200

    Reproduce the go-get bug
    $ cd .; git clone https://github.com/gorilla/mux /tmp/pytest-0/test_fetch_go_specific_github_0/src/github.com/gorilla/mux
    Cloning into '/tmp/pytest-0/test_fetch_go_specific_github_0/src/github.com/gorilla/mux'...
    fatal: unable to look up current user in the passwd file: no such user
    Unexpected end of command stream
    package github.com/gorilla/mux: exit status 128
    command ['go', 'get', '-d', 'github.com/gorilla/mux'] ended with 1

--- runtest.sh
+++ runtest.sh
@@ -96,6 +96,7 @@ echo "Starting test suite"
 docker run -t \
   -v "${here}:/f8a_worker:ro,Z" \
   --network ${DOCKER_NETWORK} \
+  -u 9007 \
   -e PGBOUNCER_SERVICE_HOST=${TESTDB_CONTAINER_NAME} \
   -e S3_ENDPOINT_URL=${S3_ENDPOINT_URL} \
   -e DEPLOYMENT_PREFIX='test' \
