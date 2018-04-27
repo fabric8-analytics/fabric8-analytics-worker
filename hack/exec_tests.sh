4983087979781ee6be6b9251a4fa87e409cf8116
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-04-27 10:41:11 +0200

    Run CC and MI on CI

--- hack/exec_tests.sh
+++ hack/exec_tests.sh
@@ -3,6 +3,20 @@
 set -e
 DIR=$(dirname "${BASH_SOURCE[0]}")
 
+echo "*****************************************"
+echo "*** Cyclomatic complexity measurement ***"
+echo "*****************************************"
+radon cc -s -a -i venv /f8a_worker/f8a_worker $@
+
+echo "*****************************************"
+echo "*** Maintainability Index measurement ***"
+echo "*****************************************"
+radon mi -s -i venv /f8a_worker/f8a_worker $@
+
+echo "*****************************************"
+echo "*** Unit tests ***"
+echo "*****************************************"
+
 # we need no:cacheprovider, otherwise pytest will try to write to directory .cache which is in /usr under unprivileged
 # user and will cause exception
 py.test -p no:cacheprovider --cov=/f8a_worker/f8a_worker --cov-report term-missing -vv "$@"
