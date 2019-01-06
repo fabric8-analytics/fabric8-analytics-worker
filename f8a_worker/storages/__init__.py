1133fe25593a8a184f4a6fc188316872f1af5ff2
Michal Srb <michal@redhat.com>
2019-01-06 16:52:00 +0100

    Delete S3GitHubManifestMetadata storage

--- f8a_worker/storages/__init__.py
+++ f8a_worker/storages/__init__.py
@@ -12,7 +12,6 @@ from .s3_package_data import S3PackageData
 from .s3_manifests import S3Manifests
 from .s3_mavenindex import S3MavenIndex
 from .s3_readme import S3Readme
-from .s3_gh_manifests import S3GitHubManifestMetadata
 from .s3_userprofilestore import S3UserProfileStore
 from .s3_description_repository import S3RepositoryDescription
 from .s3_userintent import S3UserIntent
@@ -31,7 +30,6 @@ assert S3PackageData is not None
 assert S3Manifests is not None
 assert S3MavenIndex is not None
 assert S3Readme is not None
-assert S3GitHubManifestMetadata is not None
 assert S3UserProfileStore is not None
 assert S3RepositoryDescription is not None
 assert S3UserIntent is not None
