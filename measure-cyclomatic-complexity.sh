f4c0ad6d683de6cd98b51f7b1853075c441a297e
Martin Sehnoutka <msehnout@redhat.com>
2019-04-10 14:26:15 +0200

    migrate to python3.6

--- measure-cyclomatic-complexity.sh
+++ measure-cyclomatic-complexity.sh
@@ -3,7 +3,7 @@
 function prepare_venv() {
     VIRTUALENV=$(which virtualenv)
     if [ $? -eq 1 ]; then
-        # python34 which is in CentOS does not have virtualenv binary
+        # python36 which is in CentOS does not have virtualenv binary
         VIRTUALENV=$(which virtualenv-3)
     fi
 
