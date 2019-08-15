4a03c51a746e7d8f73bf4f613cb27cabc0037e13
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-08-15 15:45:03 +0200

    Workaround for https://bugs.centos.org/view.php?id=16337

--- cico_run_pydoc.sh
+++ cico_run_pydoc.sh
@@ -3,6 +3,9 @@
 set -ex
 
 prep() {
+    # workaround for https://bugs.centos.org/view.php?id=16337 #
+    echo -e "exclude=mirror.ci.centos.org" >> /etc/yum/pluginconf.d/fastestmirror.conf
+
     yum -y update
     yum -y install epel-release
     yum -y install python36 python36-virtualenv which
