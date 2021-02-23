bfbca215067cbad6e19048b24cfbae9fcd8f2d27
Maor Friedman <maor.friedman@redhat.com>
2021-02-23 13:35:28 +0200

    Add App SRE build scripts and Dockerfiles (#950)
    * add Dockerfiles pulling from quay.io/app-sre
    
    * add build_deploy scripts
    
    * add build_deploy scripts

---
+++ build_deploy.rhel.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -exv
+
+BASE_IMG="rhel-bayesian-cucos-worker"
+QUAY_IMAGE="quay.io/app-sre/${BASE_IMG}"
+IMG="${BASE_IMG}:latest"
+
+GIT_HASH=`git rev-parse --short=7 HEAD`
+
+# login to quay to pull base image
+DOCKER_CONF="$PWD/.docker"
+mkdir -p "$DOCKER_CONF"
+docker --config="$DOCKER_CONF" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
+
+# build the image
+docker build  --no-cache \
+              --force-rm \
+              -t ${IMG}  \
+              -f ./Dockerfile.rhel.app-sre .
+
+# push the image
+skopeo copy --dest-creds "${QUAY_USER}:${QUAY_TOKEN}" \
+    "docker-daemon:${IMG}" \
+    "docker://${QUAY_IMAGE}:latest"
+
+skopeo copy --dest-creds "${QUAY_USER}:${QUAY_TOKEN}" \
+    "docker-daemon:${IMG}" \
+    "docker://${QUAY_IMAGE}:${GIT_HASH}"
