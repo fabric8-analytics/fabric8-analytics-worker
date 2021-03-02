426ca423d1009b5dee25a2a51bcea2093784f405
rafiu007 <rafiu.jaman1997@gmail.com>
2021-03-02 12:21:26 +0530

    Added column redhat_commons_id to store uuid from red hat commons.

--- alembic/env.py
+++ alembic/env.py
@@ -6,8 +6,7 @@ import os
 from alembic import context
 from sqlalchemy import engine_from_config, pool, MetaData
 from logging.config import fileConfig
-import sys 
-sys.path = ['', '..'] + sys.path[1:]
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
